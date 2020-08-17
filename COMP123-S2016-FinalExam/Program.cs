dee9298dabef5848d1d35e5b3c4f4c944c808f40
Wen Xu <wxu63@my.centennialcollege.ca>
2020-08-17 12:09:59 -0400

    Add project files.

---
+++ COMP123-S2016-FinalExam/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_M2020_FinalExam
+{
+    public static class Program
+    {
+        public static Character character = new Character();
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new AbilityGeneratorForm());
+        }
+    }
+}
