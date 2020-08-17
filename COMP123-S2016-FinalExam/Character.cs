68aa84b008504b09319c9e02d9d4ff3245289a19
Wen Xu <wxu63@my.centennialcollege.ca>
2020-08-17 12:24:40 -0400

    add firstname and lastname properites

--- COMP123-S2016-FinalExam/Character.cs
+++ COMP123-S2016-FinalExam/Character.cs
@@ -20,5 +20,9 @@ namespace COMP123_M2020_FinalExam
         public string Charisma { get; set; }
 
         public string Race { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
     }
 }
